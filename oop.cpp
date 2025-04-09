91b73fc8e01e072d77590fb28192dc3c301d40f7
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2025-04-09 23:06:46 +0500

    Testing github commit new laptop

--- oop.cpp
+++ oop.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 using namespace std;
-class Employee
-{
-public:
-    int x;
-};
 int main()
 {
-    Employee a;
-    int v = a.x = 10;
-    cout << v << endl;
     return 0;
 }
