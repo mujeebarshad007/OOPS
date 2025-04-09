d87fad568493eebc88dc808acedbc5425aca4005
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2025-04-09 23:58:54 +0500

    oops

--- oop.cpp
+++ oop.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
+class Student
+{
+
+private:
+    int regno;
+    string name;
+
+public:
+    int x;
+    void display()
+    {
+        cout << x << endl;
+    }
+};
 int main()
 {
+    Student *s1 = new Student();
+
     return 0;
 }
